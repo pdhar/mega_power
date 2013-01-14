d199a25209dede599f0326bb66decc3627780511
pdhar <pranav.dhar2@gmail.com>
2013-01-14 15:40:37 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MegaPower::Application.config.secret_token = '26583656ebc46fb656e38d705fe16b7327df5afd399ad1a259521d7cc903105a63791a5fdde00051eadd78acc1c3c6499aad1be8cb8230e135576cc412c987e1'
