c192f3665486576d19adbc889854e93566e2ff6e
pdhar <pranav.dhar2@gmail.com>
2013-01-22 00:02:13 +0400

    added default value to migration

--- db/migrate/20130119155212_add_costs_to_componentmonths.rb
+++ db/migrate/20130119155212_add_costs_to_componentmonths.rb
@@ -1,11 +1,11 @@
 class AddCostsToComponentmonths < ActiveRecord::Migration
   def change
-    add_column :componentmonths, :total_service_cost, :decimal
-    add_column :componentmonths, :total_service_parts, :decimal
-    add_column :componentmonths, :total_service_labour, :decimal
-    add_column :componentmonths, :total_break_cost, :decimal
-    add_column :componentmonths, :total_break_parts, :decimal
-    add_column :componentmonths, :total_break_labour, :decimal
+    add_column :componentmonths, :total_service_cost, :decimal, :default=>0.0, :null => false
+    add_column :componentmonths, :total_service_parts, :decimal, :default=>0.0, :null => false
+    add_column :componentmonths, :total_service_labour, :decimal, :default=>0.0, :null => false
+    add_column :componentmonths, :total_break_cost, :decimal, :default=>0.0, :null => false
+    add_column :componentmonths, :total_break_parts, :decimal, :default=>0.0, :null => false
+    add_column :componentmonths, :total_break_labour, :decimal, :default=>0.0, :null => false
   end
 end
 
