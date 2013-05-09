87acb03fb11f16b904944d3596d266bbde819dde
pdhar <pranav.dhar2@gmail.com>
2013-05-09 12:51:34 +0530

    asd

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,8 @@ class Service < ActiveRecord::Base
   attr_accessible :service_description, :service_name, :service_no, 
   :part_ids, :service_cost, :labour_ids, :total_parts_cost, :total_labour_cost
   
+  
+  
   validates_presence_of :service_description, :service_name, :service_no 
   validates_uniqueness_of :service_no
   validates_length_of :service_description, :maximum => 50
